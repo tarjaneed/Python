2afd8e7475e5648fb23f6ff68a46f78f43d31074
tarjaneed <tarjaneed@gmail.com>
2024-06-13 12:57:10 -0400

    Comments added

--- Two-Heaps.py
+++ Two-Heaps.py
@@ -23,23 +23,24 @@ large = [] # Min Heap - holds the larger values and we need min value from those
 # O(logn)
 def add(num):
     # By default we add to the small heap
-    heapq.heappush(small, -1 * num) # Since max heap  small = [-7, -1]
+    heapq.heappush(small, -1 * num) # Since max heap needs to be build so store negative values
 
-    # We need to check first if small has values less than large to move elements; then check uneven length or size to move elements
+    # We need to check first if small has values less than large to move elements; then check uneven length or size to move elements; Order of checking is important to avoid imbalance
     # Check if values in the small heap is lesser than the values in the large heap or not; if not we move the elements
-    if small and large and (-1 * small[0]) > large[0]: #  7 > 2 yes
-        val = - 1 * heapq.heappop(small) # small = [-1]
-        heapq.heappush(large, val) # large[7, 2, 3]
+    if small and large and (-1 * small[0]) > large[0]:
+        val = - 1 * heapq.heappop(small)
+        heapq.heappush(large, val)
 
     # Uneven size: Now check if difference between small and large heap exceeded 1 or not; if it did we need to re-balance and move to the root element to the large heap
-    if len(small) - len(large) > 1: # Difference is 2 or more   2 > 1 + 1 no
+    if len(small) - len(large) > 1: # Difference is 2 or more
         # Remove the root element from the small heap and add to large heap to re-balance
-        val = - 1 * heapq.heappop(small) # -2 => 2 small [-1]
-        heapq.heappush(large, val) # large = [2]
+        val = - 1 * heapq.heappop(small)
+        heapq.heappush(large, val)
 
     if len(large) - len(small) > 1:
         val = heapq.heappop(large)
         heapq.heappush(small, -1 * val)
+
 # O(1)
 def findMedian():
     # Odd who ever has larger length top of that is the median
