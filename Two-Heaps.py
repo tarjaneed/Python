ceb688336fbe543834a914686772a56de0664806
tarjaneed <tarjaneed@gmail.com>
2024-06-13 12:38:57 -0400

    Two Heaps

---
+++ Two-Heaps.py
@@ -0,0 +1,61 @@
+'''
+The median is the middle value in a sorted list of integers. For lists of even length, there is no middle value, so the median is the mean of the two middle values.
+
+For example:
+
+For arr = [1,2,3], the median is 2.
+For arr = [1,2], the median is (1 + 2) / 2 = 1.5
+
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far.
+'''
+
+# TC: O(1) to find the meadian
+
+import heapq
+
+small = [] # Max Heap - holds smaller values and we need max from those smaller values at the top
+large = [] # Min Heap - holds the larger values and we need min value from those larger values at the top
+
+# O(logn)
+def add(num):
+    # By default we add to the small heap
+    heapq.heappush(small, -1 * num) # Since max heap  small = [-7, -1]
+
+    # We need to check first if small has values less than large to move elements; then check uneven length or size to move elements
+    # Check if values in the small heap is lesser than the values in the large heap or not; if not we move the elements
+    if small and large and (-1 * small[0]) > large[0]: #  7 > 2 yes
+        val = - 1 * heapq.heappop(small) # small = [-1]
+        heapq.heappush(large, val) # large[7, 2, 3]
+
+    # Uneven size: Now check if difference between small and large heap exceeded 1 or not; if it did we need to re-balance and move to the root element to the large heap
+    if len(small) > len(large) + 1: # Difference is 2 or more   2 > 1 + 1 no
+        # Remove the root element from the small heap and add to large heap to re-balance
+        val = - 1 * heapq.heappop(small) # -2 => 2 small [-1]
+        heapq.heappush(large, val) # large = [2]
+
+    if len(large) > len(small) + 1:
+        val = heapq.heappop(large)
+        heapq.heappush(small, -1 * val)
+# O(1)
+def findMedian():
+    # Odd who ever has larger length top of that is the median
+    # If small heap has larger length then it's root is the median
+    if len(small) > len(large):
+        return -1 * small[0] # Return max value from min numbers
+    elif len(large) > len(small):
+        return large[0] # Return min value from max numbers
+    else:
+        # Even - both small and large have same length i.e. same number of elements
+        # Take max from small and min from large i.e. take top elements from both and divide it by 2 to get the median
+        return ((-1 * small[0]) + large[0]) / 2
+
+add(1)
+add(2)
+add(3)
+print(findMedian())
+add(7)
+print(findMedian())
